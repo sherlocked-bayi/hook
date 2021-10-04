7320ae052883fbe8f9094aea98247fbe6c57270d
HuQing <81566673+sherlocked-bayi@users.noreply.github.com>
2021-10-04 10:49:22 +0800

    Add files via upload

---
+++ work3/work3.h
@@ -0,0 +1,32 @@
+﻿
+// work3.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// Cwork3App:
+// 有关此类的实现，请参阅 work3.cpp
+//
+
+class Cwork3App : public CWinApp
+{
+public:
+	Cwork3App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cwork3App theApp;
